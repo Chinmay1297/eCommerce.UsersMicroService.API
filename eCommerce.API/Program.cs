2c2c88feef697b662d82a735de56b68f7c1bf681
Chinmay Prusty <kanha12297@gmail.com>
2026-05-31 02:05:14 +0530

    Added JsonSerializer option to convert enum values in request/response.

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,6 +1,7 @@
 using eCommerce.Infrastructure;
 using eCommerce.Core;
 using eCommerce.API.Middlewares;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,12 @@ builder.Services.AddInfrastructure();
 builder.Services.AddCore();
 
 //Add controllers to the service collection
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+        .AddJsonOptions((options) =>        
+        {
+            //You need to add this to convert enum values to string in the JSON request. Otherwise, the API will expect integer values for enum properties in the request body, and result in error.
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
 
 //Build the web application
 var app = builder.Build();
