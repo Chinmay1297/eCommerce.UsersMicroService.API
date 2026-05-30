1cbc34d5fa257ee4700b091dd6bad602d10d6fba
Chinmay Prusty <kanha12297@gmail.com>
2026-05-31 01:48:22 +0530

    Added User Controller

---
+++ eCommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using eCommerce.Core.DTO;
+using eCommerce.Core.ServiceContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterRequest registerRequest)
+        {
+            //Check for invalid register Request
+            if (registerRequest == null)
+            {
+                return BadRequest("Invalid registration data");
+            }
+
+            //Call the UserService to handle registration
+            AuthenticationResponse? authenticationResponse = await _userService.Registration(registerRequest);
+
+            if (authenticationResponse == null || authenticationResponse.Success == false)
+            {
+                return BadRequest(authenticationResponse);
+            }
+
+            return Ok(authenticationResponse);
+        }
+
+        [HttpPost("login")] //Combination of [HttpPost] and [Route("login")]
+        public async Task<IActionResult> Login(LoginRequest loginRequest)
+        {
+            if(loginRequest == null)
+            {
+                return BadRequest("Invalid login data");
+            }
+
+            AuthenticationResponse? authenticationResponse = await _userService.Login(loginRequest);
+
+            if(authenticationResponse == null || authenticationResponse.Success == false)
+            {
+                return BadRequest(authenticationResponse);
+            }
+
+            return Ok(authenticationResponse);
+        }
+    }
+}
